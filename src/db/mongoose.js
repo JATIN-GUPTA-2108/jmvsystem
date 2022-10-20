42b72ea11753a3417ac73b80d26f2c1f509c7343
JATIN-GUPTA-2108 <63347186+JATIN-GUPTA-2108@users.noreply.github.com>
2022-10-20 20:31:35 +0530

    Add files via upload

---
+++ src/db/mongoose.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://MAVERICK:8CBsEVoAO3yn5sHk@cluster0.20dinkc.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+})
